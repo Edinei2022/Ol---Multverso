aa8deba055865ecba6e065b9bb9f89d20ec6485e
Edinei Almeida <99333354+Edinei2022@users.noreply.github.com>
2023-11-06 18:07:58 -0300

    Update Calculadora escolar 1.0.py

--- Calculadora escolar 1.0.py
+++ Calculadora escolar 1.0.py
@@ -1,5 +1,15 @@
 ## Calculadora de mídia de Aluno ##
 
+disciplina = ["português", "inglês", "matématica", "física"]
+print(disciplina[0])
+
+for i in disciplina:
+    print(i)
+português = [1]
+inglês = [2]
+matemática = [3]
+física = [4]
+
 nota_1 = float(input("Digite a Primeira nota:"))
 nota_2 = float(input("Digite a Segunda nota:"))
 nota_3 = float(input("Digite a nota Terceira:"))
@@ -18,3 +28,4 @@ elif nota <= 5:
 
 else:
     print("Você esta em recuperação")
+
