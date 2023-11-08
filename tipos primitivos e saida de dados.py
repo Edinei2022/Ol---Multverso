27400ebe6ba2bdbbf2f9933a9ed3536ea1ce83fe
Edinei Almeida <99333354+Edinei2022@users.noreply.github.com>
2023-11-07 21:20:21 -0300

    Update tipos primitivos e saida de dados.py

--- tipos primitivos e saida de dados.py
+++ tipos primitivos e saida de dados.py
@@ -2,4 +2,5 @@ n1 = int(input("Digite um nomero:"))
 n2 = int(input("digite outro numero:"))
 
 soma = (n1 + n2)
-print("resultado vale", soma)
+# print("O resultado entre", n1, 'e', n2, 'vale', soma) #
+print("O Reultado entre {} e {} vale {}".format(n1,n2, soma))
